69d6e929363d52e889f375a0feba4b28c17aff6c
Wiciaki <wicia_6@wp.pl>
2016-01-27 00:55:37 +0100

    AllyPingSpammer upload

---
+++ AllyPingSpammer/Helper.cs
@@ -0,0 +1,130 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable once CheckNamespace
+namespace SparkTech.Helpers
+{
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    using LeagueSharp;
+    using LeagueSharp.SDK.Core.UI.IMenu;
+    using LeagueSharp.SDK.Core.UI.IMenu.Abstracts;
+    using LeagueSharp.SDK.Core.UI.IMenu.Values;
+
+    /// <summary>
+    /// Provides miscallenous methods and extensions
+    /// </summary>
+    public static class Helper
+    {
+        private static ushort i;
+
+        /// <summary>
+        /// The array containing AD Carry names
+        /// </summary>
+        private static readonly string[] ADCs =
+        {
+            "Caitlyn", "Corki", "Draven", "Ashe", "Ezreal", "Graves", "Jinx",
+            "Kalista", "Kog'Maw", "Lucian", "Miss Fortune", "Quinn", "Sivir",
+            "Teemo", "Tristana", "Twitch", "Varus", "Vayne"
+        };
+
+        /// <summary>
+        /// Gets the real champion name
+        /// </summary>
+        /// <param name="champ"><see cref="Obj_AI_Hero"/> instance</param>
+        /// <returns></returns>
+        public static string ChampionName(this Obj_AI_Hero champ)
+        {
+            var name = champ.ChampionName;
+
+            switch (name.ToLower())
+            {
+                case "chogath":
+                    return "Cho'Gath";
+                case "drmundo":
+                    return "Dr. Mundo";
+                case "khazix":
+                    return "Kha'Zix";
+                case "kogmaw":
+                    return "Kog'Maw";
+                case "reksai":
+                    return "Rek'Sai";
+                case "velkoz":
+                    return "Vel'Koz";
+                default:
+                    return name.Space();
+            }
+        }
+
+        /// <summary>
+        /// Spaces the input string
+        /// </summary>
+        /// <param name="input">The string to be spaced</param>
+        /// <param name="ignoreAcronyms">If <c>true</c>, ignore acronyms</param>
+        /// <returns></returns>
+        public static string Space(this string input, bool ignoreAcronyms = true)
+        {
+            return ignoreAcronyms
+                       ? Regex.Replace(input, "((?<=\\p{Ll})\\p{Lu})|((?!\\A)\\p{Lu}(?>\\p{Ll}))", " $0")
+                       : Regex.Replace(input, "(?<!^)([A-Z])", " $1");
+        }
+
+        /// <summary>
+        /// Determines whether this <see cref="Obj_AI_Hero"/> instance is an AD Carry
+        /// </summary>
+        /// <param name="hero"><see cref="Obj_AI_Hero"/> instance</param>
+        /// <returns></returns>
+        public static bool IsADC(this Obj_AI_Hero hero)
+        {
+            return ADCs.Contains(hero.ChampionName());
+        }
+
+        /// <summary>
+        /// Converts this string instance usable for the menu
+        /// </summary>
+        /// <param name="input">The <see cref="string"/> instance</param>
+        /// <returns></returns>
+        public static string ToMenuUse(this string input)
+        {
+            return input.Space().ToLower().Replace('\'', ' ').Trim().Replace(' ', '_');
+        }
+
+        /// <summary>
+        /// Gets the menu separator text
+        /// </summary>
+        public static string SeparatorText => $"st_separator_{++i}";
+
+        /// <summary>
+        /// Adds the separator to the specified <see cref="Menu"/> instance
+        /// </summary>
+        /// <param name="menu"><see cref="Menu"/> instance</param>
+        /// <param name="text">The custom text to be displayed</param>
+        public static void AddSeparator(this Menu menu, string text = null)
+        {
+            menu.Add(new MenuSeparator(SeparatorText, text ?? string.Empty));
+        }
+
+        /// <summary>
+        /// Determined whether the game is funning in FullHD
+        /// </summary>
+        /// <returns></returns>
+        public static bool FullHD()
+        {
+            return Drawing.Height == 1920 && Drawing.Width == 1080;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="AMenuComponent"/> globally
+        /// </summary>
+        /// <typeparam name="TMenuComponent">The desired type</typeparam>
+        /// <param name="name">The <see cref="AMenuComponent"/> name</param>
+        /// <returns></returns>
+        public static TMenuComponent GetGlobally<TMenuComponent>(string name) where TMenuComponent : AMenuComponent
+        {
+            return
+                (TMenuComponent)
+                MenuManager.Instance.Menus.Where(menu => menu.Root)
+                    .SelectMany(rootmenu => rootmenu.Components.Values) //.ToHashSet()
+                    .FirstOrDefault(component => component.Name == name && component is TMenuComponent);
+        }
+    }
+}
---
+++ AllyPingSpammer/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace AllyPingSpammer
+{
+    using System;
+    using System.Linq;
+
+    using LeagueSharp;
+    using LeagueSharp.SDK;
+    using LeagueSharp.SDK.Core.UI.IMenu;
+    using LeagueSharp.SDK.Core.UI.IMenu.Values;
+    using LeagueSharp.SDK.Core.Utils;
+
+    using SparkTech.Helpers;
+
+    /// <summary>
+    /// The <see cref="Program"/> class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The beginning for every menu item
+        /// </summary>
+        private const string Header = "st_ally_ping";
+
+        /// <summary>
+        /// The <see cref="Random"/> instance
+        /// </summary>
+        private static readonly Random Random = new Random(Variables.TickCount);
+
+        /// <summary>
+        /// The entry point
+        /// </summary>
+        /// <param name="args">The empty string <see cref="Array"/></param>
+        private static void Main(string[] args)
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            Events.OnLoad += delegate
+                {
+                    bool shouldRandomize = false, shouldAssign = false;
+
+                    var root = new Menu(Header, "[ST] Ally Ping Spammer", true).Attach();
+
+                    var active = root.Add(new MenuBool(Header + "_active", "Active"));
+
+                    var hero =
+                        root.Add(
+                            new MenuList<string>(
+                                Header + "_hero",
+                                "Ally to be spammed",
+                                GameObjects.AllyHeroes.Where(ally => !ally.IsMe).Select(ally => ally.ChampionName())));
+
+                    var delay = root.Add(new MenuSlider(Header + "_delay", "Delay between attempts", 800, 200, 5000));
+
+                    var randomizer = root.Add(new MenuBool(Header + "_randomize", "^ Randomize delay"));
+
+                    var ping = root.Add(new MenuList<PingCategory>(Header + "_pingtype", nameof(PingCategory)));
+
+                    root.AddSeparator("Made by Spark");
+
+                    var operation = new TickOperation(delay.Value, delegate
+                            {
+                                if (!active.Value)
+                                {
+                                    return;
+                                }
+
+                                var position =
+                                    GameObjects.AllyHeroes.FirstOrDefault(
+                                        champ => champ.ChampionName() == hero.SelectedValue && !champ.IsMe)?.Position;
+
+                                if (position.HasValue)
+                                {
+                                    Game.SendPing(ping.SelectedValue, position.Value);
+                                }
+                                
+                                if (randomizer.Value)
+                                {
+                                    shouldRandomize = true;
+                                }
+                            });
+
+                    delay.ValueChanged += delegate
+                        { shouldAssign = true; };
+
+                    Game.OnUpdate += delegate
+                        {
+                            if (shouldRandomize)
+                            {
+                                operation.TickDelay = delay.Value = Random.Next(200, 5000);
+                                shouldRandomize = false;
+                            }
+
+                            if (!shouldAssign)
+                            {
+                                return;
+                            }
+
+                            operation.TickDelay = delay.Value;
+                            shouldAssign = false;
+                        };
+                };
+        }
+    }
+}
