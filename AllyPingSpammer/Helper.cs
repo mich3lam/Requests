0e67d0eb8a860d7bcd97b12663c41d061e24927a
Wiciaki <wicia_6@wp.pl>
2016-01-27 23:59:22 +0100

    Removed the redundant methods

--- AllyPingSpammer/Helper.cs
+++ AllyPingSpammer/Helper.cs
@@ -3,12 +3,10 @@
 namespace SparkTech.Helpers
 {
     using System;
-    using System.Linq;
     using System.Text.RegularExpressions;
 
     using LeagueSharp;
     using LeagueSharp.SDK.Core.UI.IMenu;
-    using LeagueSharp.SDK.Core.UI.IMenu.Abstracts;
     using LeagueSharp.SDK.Core.UI.IMenu.Values;
 
     using SharpDX;
@@ -21,16 +19,6 @@ namespace SparkTech.Helpers
         private static Random Random => AllyPingSpammer.Program.Random;
 
         private static ushort i;
-
-        /// <summary>
-        /// The array containing AD Carry names
-        /// </summary>
-        private static readonly string[] ADCs =
-        {
-            "Caitlyn", "Corki", "Draven", "Ashe", "Ezreal", "Graves", "Jinx",
-            "Kalista", "Kog'Maw", "Lucian", "Miss Fortune", "Quinn", "Sivir",
-            "Teemo", "Tristana", "Twitch", "Varus", "Vayne"
-        };
 
         /// <summary>
         /// Gets the real champion name
@@ -74,26 +62,6 @@ namespace SparkTech.Helpers
         }
 
         /// <summary>
-        /// Determines whether this <see cref="Obj_AI_Hero"/> instance is an AD Carry
-        /// </summary>
-        /// <param name="hero"><see cref="Obj_AI_Hero"/> instance</param>
-        /// <returns></returns>
-        public static bool IsADC(this Obj_AI_Hero hero)
-        {
-            return ADCs.Contains(hero.ChampionName());
-        }
-
-        /// <summary>
-        /// Converts this string instance usable for the menu
-        /// </summary>
-        /// <param name="input">The <see cref="string"/> instance</param>
-        /// <returns></returns>
-        public static string ToMenuUse(this string input)
-        {
-            return input.Space().ToLower().Replace('\'', ' ').Trim().Replace(' ', '_');
-        }
-
-        /// <summary>
         /// Gets the menu separator text
         /// </summary>
         public static string SeparatorText => $"st_separator_{++i}";
@@ -106,15 +74,6 @@ namespace SparkTech.Helpers
         public static void AddSeparator(this Menu menu, string text = null)
         {
             menu.Add(new MenuSeparator(SeparatorText, text ?? string.Empty));
-        }
-
-        /// <summary>
-        /// Determined whether the game is funning in FullHD
-        /// </summary>
-        /// <returns></returns>
-        public static bool FullHD()
-        {
-            return Drawing.Height == 1920 && Drawing.Width == 1080;
         }
 
         /// <summary>
@@ -144,21 +103,6 @@ namespace SparkTech.Helpers
             var random = Random.Next(0, maxDiff);
 
             return b ? input + random : input - random;
-        }
-
-        /// <summary>
-        /// Gets the <see cref="AMenuComponent"/> globally
-        /// </summary>
-        /// <typeparam name="TMenuComponent">The desired type</typeparam>
-        /// <param name="name">The <see cref="AMenuComponent"/> name</param>
-        /// <returns></returns>
-        public static TMenuComponent GetGlobally<TMenuComponent>(string name) where TMenuComponent : AMenuComponent
-        {
-            return
-                (TMenuComponent)
-                MenuManager.Instance.Menus.Where(menu => menu.Root)
-                    .SelectMany(rootmenu => rootmenu.Components.Values) //.ToHashSet()
-                    .FirstOrDefault(component => component.Name == name && component is TMenuComponent);
         }
     }
 }
